718439a5e8f86fd1b3e23469abcac52f8414f20f
lukeeeeeee334 <lukeosullivan123@gmail.com>
2024-11-07 13:37:09 +0000

    added class variables for the web service interface
    :)

--- TicTacToeClient/src/main/java/com/groupproject/tictactoeclient/TicTacToeClient.java
+++ TicTacToeClient/src/main/java/com/groupproject/tictactoeclient/TicTacToeClient.java
@@ -2,13 +2,18 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
 
+
 package com.groupproject.tictactoeclient;
+import com.tttws.TicTacToeWS;
+import com.tttws.TicTacToeWebService;
 
 /**
  *
  * @author Willi
  */
 public class TicTacToeClient {
+    private static TicTacToeWebService service;
+    private static TicTacToeWS proxy;
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
