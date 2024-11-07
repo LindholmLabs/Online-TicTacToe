4a84ce114eec4b6818ec7f1f378c08a188cf61c5
William Lindholm <william_lindholm@outlook.com>
2024-11-07 12:47:55 +0000

    Imported project files, and created maven project.

---
+++ TicTacToeClient/src/main/java/com/groupproject/tictactoeclient/TicTacToeClient.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.groupproject.tictactoeclient;
+
+/**
+ *
+ * @author Willi
+ */
+public class TicTacToeClient {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
