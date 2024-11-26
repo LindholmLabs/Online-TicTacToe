6df8d41f340f417db2657e679b8b7c4d9bf72cc2
adglas21 <adamglasheen99@gmail.com>
2024-11-26 19:24:48 +0000

    Implemented, check who wins, reset the game  after winner

--- TicTacToeClient/src/main/java/com/groupproject/tictactoeclient/PollingThread.java
+++ TicTacToeClient/src/main/java/com/groupproject/tictactoeclient/PollingThread.java
@@ -4,8 +4,10 @@
  */
 package com.groupproject.tictactoeclient;
 
+import com.groupproject.tictactoeclient.ContentPanes.MainContentPanel;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -14,6 +16,9 @@ import java.util.logging.Logger;
 public class PollingThread implements Runnable {
     private TicTacToeClient client;
     private HelperMethods helperMethods;
+    
+    //flag used to keep track if game is over so the dialog box doesnt keep appearing
+    private boolean gameOver = false;
     
     public PollingThread(TicTacToeClient client) {
         this.client = client;
@@ -36,6 +41,41 @@ public class PollingThread implements Runnable {
                 client.openGames = client.proxy.showOpenGames();
                 System.out.println("Result: " + client.openGames);
                 client.refreshCurrentPanel();
+                
+                
+                //call the web server to check if the game has been won or not yet
+                 String result = client.proxy.checkWin(Integer.parseInt(client.GID));
+       
+        //if the game is not over check the result of the game
+        if(!gameOver) {
+        switch (result) {
+            case "1":
+                // Player 1 wins
+                JOptionPane.showMessageDialog(client.frame, "Player 1 wins!");    
+                helperMethods.showOptions();
+                gameOver = true;
+                break;
+            case "2":
+                // Player 2 wins
+                JOptionPane.showMessageDialog(client.frame, "Player 2 wins!");
+                helperMethods.showOptions();
+                gameOver = true;
+                break;
+            case "3":
+                // It's a draw
+                JOptionPane.showMessageDialog(client.frame, "It's a draw!");
+                helperMethods.showOptions();
+                gameOver = true;
+                break;
+            case "0":
+                // Game continues
+                break;
+            default:
+                JOptionPane.showMessageDialog(client.frame, "Error checking the game status.");
+                
+        }
+        }
+                
             } catch (Exception e) {
                 e.printStackTrace();
             }  
